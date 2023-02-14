9fc45bb213b692e92250df7b3c42f8d98e7a8547
Tanvir Hassan <mthnies@gmail.com>
2023-02-14 01:33:13 -0500

    -- added catalog api and docker compose project

---
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -0,0 +1,35 @@
+using Catalog.API.Data.Interfaces;
+using Catalog.API.Data;
+using Catalog.API.Repositories.Interfaces;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//My Services
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
