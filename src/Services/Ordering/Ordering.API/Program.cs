6b4851ccb86951eba7ff18bd898b34a9850e299c
Tanvir Hassan <mthnies@gmail.com>
2023-04-09 05:17:32 -0400

    added order api service

---
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.Extensions.Configuration;
+using Ordering.API.Extensions;
+using Ordering.Application;
+using Ordering.Infrastructure;
+using Ordering.Infrastructure.Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//Mine
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MigrateDatabase<OrderContext>((context, services) =>
+{
+    var logger = services.GetService<ILogger<OrderContextSeed>>();
+    OrderContextSeed.SeedAsync(context, logger).Wait();
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
+using System.Reflection;
+
+namespace Ordering.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            //will look for classes that is inherited from Profile class
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            //will look for classes that is inherited from AbstractValidator class
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            //will look for classes that is inherited from IRequestHandler & IRequest class
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            return services;
+        }
+    }
+}
